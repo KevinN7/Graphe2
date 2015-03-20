86876bbaba8e3cdd068bbdc14437615b62a2fbbf
ECCLI kevin <eccli.kevin@gmail.com>
2015-03-20 17:07:21 +0100

    Compilation OK

--- projet.ml
+++ projet.ml
@@ -8,14 +8,6 @@ let liste_v g = (fold_vertex (fun v qt -> v::qt)
                                [])
 ;;
 
-(* fonction qui liste les sommets et leur label *)
-(* liste_v: Digraph.t -> int*int list *)
-let liste_vl g = (fold_vertex (fun v qt -> (V.label v,Mark.get v)::qt) 
-                               g
-                               [])
-;;
-
-
 (* fonction qui liste les aretes *)
 (* liste_e: Digraph.t -> int*int list *) 
 let liste_e g = 
@@ -23,16 +15,6 @@ let liste_e g =
                      g
                      [])
 ;;
-
-(* fonction qui liste les aretes et leur label *)
-(* liste_e: Digraph.t -> int*int*int list *) 
-let liste_el g = 
-        (fold_edges  (fun v1 v2 qt -> (V.label v1,V.label v2,E.label (find_edge g v1 v2))::qt) 
-                     g
-                     [])
-;;
-
-(* fct liste les predecesseur*)
 
 (*enlever liste_pred*)
 let liste_pred v g =
@@ -46,6 +28,9 @@ let appartient v l =
 
 let inclu ens1 ens2 =
   List.fold_right (fun v b -> (appartient v ens2) && b) ens1 true;;
+
+
+type trace = (DAG.vertex list) list;;
 
  (* entrees: 
    - un DAG
@@ -69,25 +54,8 @@ let tri_topologique g =
 (* trace d'execution 
    definie en Section 2 de l'enonce (voir Figure 2)
 *)
-(*type Trace = (Vertex list) list ;;*)		 
+		 
 		 			
-(*let ordonnanceur_ressources_illimitees g =
-  let y = sans_dependance g in
-		let rec iter1 y z res1 =
-			match y with
-			|t::q->
-				let rec iter2 ycourant yfutur z res = 
-					match ycourant with
-					|t::q -> 
-						let newz = t::z in
-							let newy  = fold_succ (fun v l -> if(inclu (liste_pred v g) newz) then l@[v] else l) g t q
-						in iter2 q yfutur@newy newz res@[t]
-					|[] -> res::iter1
-				in (iter2 y [] z [])::res1
-			|[] -> res1
-		in iter1 y [] [];;*)
-		
-
 let etage y z g =
 	let rec iter ycourant yfutur z res = 
 		match ycourant with
@@ -121,17 +89,6 @@ let ordonnanceur_ressources_illimitees g =
 		|[] -> (resultat,yfutur,z)
 	in iter y [] z [] nbres;;
 	
-	(*let etage_res y z nbres=
-	let rec iter ycourant yfutur z resultat nbres = 
-		match ycourant with
-		|t::q -> 
-			if(nbres- Vertex.mass t  >=0) then
-				let newz=t::z and newy  = fold_succ (fun v l -> if(inclu (liste_pred v g) newz) then l@[v] else l) g t yfutur
-				in iter q newy newz resultat@[t] (nbres-  Vertex.mass t)
-			else iter q yfutur@[t] z resultat nbres
-		|[] -> (resultat,yfutur,z)
-	in iter y [] z [] nbres;;*)
-
 (* entrees: 
    - un nombre entier de ressources
    - un DAG
@@ -191,16 +148,6 @@ let ordonnanceur_ressources_limitees_avec_heuristique nbres g =
   in iter (sans_dependance g) [] [];;
    
    
-(*
-let ordonnanceur_ressources_limitees_avec_heuristique nbres g = 
-  let rec iter y z res =
-	match y with
-	|[]-> res
-	|_ -> let (resetage,yfutur,newz) = etage_res y z nbres in 
-	let yfutur_sort =List.sort (fun a b -> if(Vertex.mass a > Vertex.mass b)then -1 else(if(Vertex.mass a = Vertex.mass b)then 0 else 1)) in
-		iter yfutur_sort newz res@[resetage]
-  in iter (sans_dependance g) [] [];;*)
-
   (*creer un noeud de nom spécifié et l'ajoute au graphe*)
 let aux nom g =
 	let a=V.create(nom,1) in add_vertex g a;a;;
@@ -216,10 +163,6 @@ let prim g n nom =
                 iter (n-1) tmp
 	in iter (n-1) prem;;
 
-(*let routine g =
-	let res = g in iter_vertex (v-> ) res;res;;
-	
-	sommet_a_traiter = liste_v res*)
 
 let relier_pred g lpred noeud = 
 	List.iter (fun v->add_edge g v noeud) lpred;;
@@ -227,28 +170,37 @@ let relier_pred g lpred noeud =
 let relier_succ g lsucc noeud =
 	List.iter (fun v-> add_edge g noeud v) lsucc;; 
 
+
+let liste_edge g =
+  fold_edges (fun u v l -> (u,v)::l) g [];; 
+
+let aux_routine g pred succ =
+  (*let nom=DAG.Display.vertex_name pred in*)
+  let nom= fst(V.label pred) in
+  let rec iter n =
+    match n with
+    |0-> ()
+    |_-> let u=aux (nom^(string_of_int n)) g in
+           add_edge g pred u;
+           add_edge g u succ;
+           iter (n-1)
+  in iter (snd (V.label pred))
+;;
+
+
 let routine g =
-	let resultat = g in
-	let l = liste_v resultat in
+	let resultat = copy g in
+	let l = liste_edge resultat in
 	List.iter (
-		fun v-> 
-			let su = succ g v and pr=pred g v and
-                        na=DAG.Display.vertex_name v and
-                        poid=Vertex.mass(V.label v) in
-			let (premier,dernier) = prim resultat poid na in
-				relier_succ resultat pr dernier;
-                                relier_pred resultat su premier
+	        fun (u,v)->
+                  begin
+                  remove_edge g u v;
+                  aux_routine g u v
+                  end
 	) l;
 	resultat;;
 
 
-(*faire cpie de g*)
-
-(*recup nbr res, pred,succ*)
-(*Appliquer pour chaque noeud prim*)
-(*reconecter les pred et succ sur le resultat de *)
-
-  
 (* entrees: 
    - un nombre entier de ressources
    - un DAG
@@ -258,28 +210,37 @@ let routine g =
    - le DAG est suppose pondere (section 2.3)
    - les ressources sont supposees limitees 
    *)
-(*
-let ordonnanceur_graphe_pondere resDispo g =
-  let rec iter y z res =
-	match y with
-	|[]-> res
-	|_ -> let yordre = List.sort (fun a b -> let pa=prof_max a and pb=prof_max b in if pa>pb then 1 else if pa<pb then -1 else 0) y in 
-			let (resetage,yfutur,newz) = etage_res_pond yordre z nbres g in iter yfutur newz res@[resetage]
-  in iter (sans_dependance g) [] [];;
-  
-  
+
+let init_ordo g =
+  iter_vertex (fun v -> Mark.set v (Vertex.mass(V.label v))) g;;
+
+
 let etage_res_pond y z nbres g=
 	let rec iter ycourant yfutur z resultat nbres = 
 		match ycourant with
 		|t::q -> 
-			let poid = mass t in
-				if(nbres-mass>=0) then
+			let poid = Mark.get t in
+				if(nbres-poid>=0) then
 					let newz=t::z in
 						let newy  = fold_succ (fun v l -> if(inclu (liste_pred v g) newz) then l@[v] else l) g t yfutur
-					in iter q newy newz (resultat@[t]) (nbres-mass)
-				else (resultat,yfutur@ycourant,z)
+					in iter q newy newz (resultat@[t]) (nbres-poid)
+				else if(nbres>0) then
+                                begin
+                                        Mark.set t (poid-nbres);
+					(resultat,yfutur@ycourant,z)
+                                end
+                                else (resultat,yfutur@ycourant,z)
 		|[] -> (resultat,yfutur,z)
 	in iter y [] z [] nbres;;
 
-*)
+
+
+let ordonnanceur_graphe_pondere resDispo g =
+  init_ordo g;
+  let rec iter y z res =
+	match y with
+	|[]-> res
+	|_ -> let yordre = List.sort (fun a b -> let pa=prof_max a and pb=prof_max b in if pa>pb then 1 else if pa<pb then -1 else 0) y in 
+			let (resetage,yfutur,newz) = etage_res_pond yordre z resDispo g in iter yfutur newz res@[resetage]
+  in iter (sans_dependance g) [] [];;
 
